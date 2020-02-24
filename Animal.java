36c73aae615fd2cf9f5c7c5b26bfbe7d5f14ad64
Pine9 <emmabadelmann@gmail.com>
2020-02-24 13:30:57 -0500

    first commit!

---
+++ Animal.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Animal {
+	private String name;
+	private int legs;
+
+	public Animal(String name, int legs) {
+		this.name = name;
+		this.legs = legs;
+	}
+
+	public int getLegs() {
+		return this.legs;
+	}
+
+	public String toString() {
+		return this.name;
+	}
+}
---
+++ Dog.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+class Dog extends Animal {
+	public Dog(String name, int legs) {
+		super(name, legs);
+	}
+}
